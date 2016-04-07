3ad490b9557989d37358ccb3546318e6ae006793
greg <greg@home.pl>
2016-04-07 21:44:24 +0200

    compiler.h is being included using force include option

--- windows/compiler.h
+++ windows/compiler.h
@@ -1,6 +1,6 @@
 // Part of dump1090, a Mode S message decoder for RTLSDR devices.
 //
-// winstubs.h: windows stubs for some POSIX functions
+// compiler.h: windows stubs for compile POSIX functions
 //
 // Copyright (c) 2016 Grzegorz Suder <suder.grzegorz@gmail.com>
 //
@@ -17,38 +17,5 @@
 // You should have received a copy of the GNU General Public License  
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#ifndef WINSTUBS_H
-#define WINSTUBS_H
-
-#include <WinSock2.h>
-#include <ws2tcpip.h>
-#include <Windows.h>
-#include "pthread.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <winsock.h>
-#include <math.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <stdint.h>
-#include "clock_nanosleep\clock_nanosleep.h"
-#include <BaseTsd.h>
-
-typedef SSIZE_T ssize_t;
-typedef int socklen_t;
-
-// File
-#define STDIN_FILENO 0
-#define STDOUT_FILENO 1
-#define PATH_MAX FILENAME_MAX
-#define realpath(N,R) _fullpath((R),(N),_MAX_PATH)
-
-// Time
-#define usleep(x) Sleep(x/1000)
-#define sleep(x) Sleep(x)
-
-// Other
-#define snprintf _snprintf
-
-#endif
+#define __attribute__(x) 
+#define inline __inline
